5860bc10caa7f267a5c322fba093e392ac07c942
Bhuvaneshwara Raja <bhuvaneshwara.raja@ei.study>
2023-01-02 00:09:45 +0530

    Test Summary example

---
+++ text-sumarry/summary-text/summary-text/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace summary_text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var text = "The color of animals is by no means a matter of chance; it depends on many considerations, but in the majority of cases tends to protect the animal from danger by rendering it less conspicuous";
+        var summaryText = StringUtilities.summary_text(text,50);
+
+        Console.WriteLine(summaryText);
+    }
+}
+
---
+++ text-sumarry/summary-text/summary-text/StringUtilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace summary_text
+{
+	public class StringUtilities
+	{
+		public StringUtilities()
+		{
+		}
+
+		public static string summary_text(String text, int maxLength = 20)
+		{
+
+            var summaryList = new List<String>();
+
+            if (text.Length < maxLength)
+                return text;
+
+            else
+            {
+                var wordList = text.Split(" ");
+                var totalCount = 0;
+                foreach (var word in wordList)
+                {
+                    summaryList.Add(word);
+
+                    totalCount += word.Length + 1;
+                    if (totalCount > maxLength)
+                        break;
+
+                }
+            }
+            return String.Join(" ", summaryList) + "...";
+        }
+	}
+}
+
